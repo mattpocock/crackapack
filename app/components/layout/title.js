86c13b813cd89f662e16985982d98026db7084c5
mattpocock <mattpocockvoice@gmail.com>
2017-12-23 14:45:07 +0000

    Title Stuff

--- app/components/layout/title.js
+++ app/components/layout/title.js
@@ -6,7 +6,7 @@ class Title extends React.Component {
             <div className="row title">
             <h1 className="">Crack A Pack</h1>
             <h4 className="">Guilt-Free Cardboard Crack</h4>
-            {this.props.chooseBooster && <h2 className="align-center">Choose A Booster</h2>}
+            {this.props.chooseBooster && <div><h2 className="align-center">Choose A Booster</h2><hr/></div>}
           </div>
         )
     }
